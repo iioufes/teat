6532ea03424717d9917c5be557957982cbe01929
iioufes <52012775+iioufes@users.noreply.github.com>
2019-06-20 20:20:34 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("590996381599268901")
+let channel =     client.channels.get("591304874268950570")
 setInterval(function() {
 channel.send(`$daily`);
 }, 30)
